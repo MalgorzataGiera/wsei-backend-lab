a0427b43873656510a5fcd8dfe0d3e50074c13d1
unknown <gosgie02@wp.pl>
2024-03-14 00:21:25 +0100

    init

---
+++ BlazorApp/Data/UserService.cs
@@ -0,0 +1,31 @@
+﻿
+namespace BlazorApp.Data
+{
+    public class UserService
+    {
+        public Dictionary<string, string> Users = new Dictionary<string, string>();
+        public void Add(string connectionId, string username)
+        {
+            Users.Add(connectionId, username);
+        }
+        public void RemoveByName(string username)
+        {
+            var x = Users.First(x => x.Value == username);
+            Users.Remove(x.Key);
+        }
+        public string GetConnectionIdByName(string username)
+        {
+            string name = "";
+            foreach (var u in Users)
+            {
+                if (u.Value == username)
+                    name = u.Key;
+            }
+            return name;
+        }
+        public Dictionary<string, string> GetAll()
+        {
+            return Users;
+        }
+    }
+}
